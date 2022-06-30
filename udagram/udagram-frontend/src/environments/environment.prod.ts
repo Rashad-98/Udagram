383a7834f6194872c2d6303f0093581024699bbf
Abdelrahman_Rashad <a.rashaad98@gmail.com>
2022-06-30 19:58:12 +0200

    updates relevant to linting

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
-  apiHost: "http://udagramapi-env-1.eba-nbacdt4p.us-east-1.elasticbeanstalk.com/api/v0",
+  appName: 'Udagram',
+  apiHost: 'http://udagramapi-env-1.eba-nbacdt4p.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
